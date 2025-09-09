c8fcc36921a51bb28e187266ea2b73b87e9e445f
Marks Bel <belehow.m.564717649@gmail.com>
2025-09-09 16:53:18 +0300

    full_workflow_3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['84.201.136.206', 'localhost', '127.0.0.1', 'taskimark.ddns.net']
+ALLOWED_HOSTS = ['84.201.136.206', 'localhost', '127.0.0.1', 'marks.myvnc.com']
 
 
 # Application definition
